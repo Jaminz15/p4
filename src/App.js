224c9a545de8a1cae2fba25d68d0bfc27e0427a7
Jamin <jzimmerman15@madisoncollege.edu>
2024-12-01 18:27:41 -0600

    modified the App.js to Updated Project4-Jamin Zimmerman

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Project4-Jamin Zimmerman</h1>
+      <h1>Updated Project4-Jamin Zimmerman</h1>
     </div>
   );
 }
